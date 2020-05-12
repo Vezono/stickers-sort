16df741fc59ba87102baff11229a32ceff8c65b5
Gbball <48598980+Vezono2@users.noreply.github.com>
2020-05-12 15:25:16 +0300

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, url_for
+from flask import Flask, render_template, request, session, url_for, redirect
 app = Flask(__name__)
 import os
 
