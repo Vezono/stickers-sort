70346db9966a8fe426780c5f5905ed1362eca52d
Gbball <48598980+Vezono2@users.noreply.github.com>
2020-05-12 11:13:50 +0300

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,3 @@
+creator = 792414733
+token = '1088457782:AAHnoZ1VBbewxcrv1ai-eKqdMaPVEfD50RE'
+database = 'mongodb+srv://gbball:tttttt000!@database-fcfmt.gcp.mongodb.net/test?retryWrites=true&w=majority'
