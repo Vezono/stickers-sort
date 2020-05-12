45480a7152deff94a6a94916ae4db02f543afc7d
Gbball <48598980+Vezono2@users.noreply.github.com>
2020-05-12 14:08:19 +0300

    Update app.py

--- web/app.py
+++ web/app.py
@@ -4,7 +4,12 @@ import os
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    return render_template('index.html')
+    user_id = request.args.get('id')
+    key = request.args.get('session')
+    if not user_id or not key:
+        return = 'Вы не вошли в систему. Получите ссылку в боте командой /login'
+    return 'Аргументы есть!'
+    #return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(threaded=True, port=os.environ['PORT'])
