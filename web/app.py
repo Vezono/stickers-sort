27d4bb87c6969897c0035799af44b338528cda22
Gbball <48598980+Vezono2@users.noreply.github.com>
2020-05-12 16:25:54 +0300

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, session, url_for, redirect
 app = Flask(__name__)
 import os
+import requests
 
 import hashlib
 import redis
@@ -9,6 +10,12 @@ redis_url = os.getenv('REDISTOGO_URL')
 urlparse.uses_netloc.append('redis')
 url = urlparse.urlparse(redis_url)
 r = redis.Redis(host=url.hostname, port=url.port, db=0, password=url.password)
+
+
+from pymongo import MongoClient
+users = db.stickers.users
+
+from bot.bot import bot
 
 domain = 'http://lk-contest.herokuapp.com'
 
@@ -34,13 +41,39 @@ def logout():
 
 @app.route('/main')
 def main_page():
-    return f'Ваш айди  - {session["id"]}'
-
+    user_id = int(session["id"])
+    stickers = get_stickers(user_id)
+    imgs = []
+    for sticker in stickers:
+        sticker_url = bot.get_file_url(sticker)
+        print(sticker_url)
+    return 'Грузим ваши стикоры...'
+        
+    
+    
+    
 def check_user(user_id, key):
     salted = user_id + r.get('salt').decode("utf-8")
     true_key = hashlib.md5(salted.encode()).hexdigest()
     return (key == true_key)
     
+    
+def get_file(url, name):
+    r = requests.get(img)
+    out = open(f"res/{name}.png", "wb")
+    out.write(p.content)
+    out.close()
+    return f'res/{name}.png'
+
+
+def get_stickers(user_id):
+    user = users.find_one({'id': user_id})
+    stickers = []
+    for category in user['stickers']:
+        for sticker in user['stickers']['category']:
+            stickers.append(bot.get_file_url(sticker))
+    return stickers
+        
 if __name__ == "__main__":
     app.run(threaded=True, port=os.environ['PORT'])
     
