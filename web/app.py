73b60cb045502cb6945b1c58deaf9703cc1978f1
Gbball <48598980+Vezono2@users.noreply.github.com>
2020-05-12 13:49:52 +0300

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 import os
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    return 'Hello World!'
+    return render_template('hello.html')
 
 if __name__ == "__main__":
     app.run(threaded=True, port=os.environ['PORT'])
