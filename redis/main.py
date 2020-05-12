cc02eb55a0dc289364b589f4c6175c49d667da2f
Gbball <48598980+Vezono2@users.noreply.github.com>
2020-05-12 11:01:34 +0300

    Update main.py

--- redis/main.py
+++ redis/main.py
@@ -1,4 +1,19 @@
 import redis
 import random
+import hashlib
 import time
 r = redis.Redis()
+  
+def set_salt():
+  global r
+  salt = generate_salt()
+  r.mset({'salt': salt})
+  
+def generate_salt():
+  tth = str(time.time()).encode()
+  h = hashlib.sha1(tth).hexdigest()
+  return h[:20]
+
+while True:
+  time.sleep(random.randint(180, 1800))
+  set_salt()
