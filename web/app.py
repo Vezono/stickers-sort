2e5dff470731d20ba8cbe6cf72040de2229b5b6c
Gbball <48598980+Vezono2@users.noreply.github.com>
2020-05-12 18:01:50 +0300

    Update app.py

--- web/app.py
+++ web/app.py
@@ -64,7 +64,7 @@ def check_user(user_id, key):
 def get_file(url, name):
     r = requests.get(url)
     out = open(f"res\\{name}", "wb")
-    out.write(p.content)
+    out.write(r.content)
     out.close()
     return f'res/{name}.png'
 
