566ba2b8d106af1653e8c4235d5e68c7541416f9
Gbball <48598980+Vezono2@users.noreply.github.com>
2020-05-12 11:24:07 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -7,11 +7,35 @@ from timeit import default_timer as timer
 start_time = timer()
 from modules.BotRunner import BotsRunner
 
+from modules.heroku import Heroku
+app = Heroku(bot).app
+
 from bot import bot as stickerbot
 
 bots = {
     'Stickerbot': stickerbot
 }
+
+@bot.message_handler(commands=['reboot'])
+def reboot(m):
+    if not m.from_user.id == config.creator:
+        return
+    bot.report('Перезагрузка...')
+    app.restart()
+
+
+@bot.message_handler(commands=['logs'])
+def reboot(m):
+    if not m.from_user.id == config.creator:
+        return
+    count = 20
+    if m.text.count(' '):
+        count = int(m.text.split()[1])
+    logs = ''
+    for log in app.get_log(lines=count).split('\n'):
+        logs += '\n' + log[33:]
+    bot.reply_to(m, logs)
+
 
 runner = BotsRunner(retries=3, show_traceback=True)
 runner.add_bots(bots)
