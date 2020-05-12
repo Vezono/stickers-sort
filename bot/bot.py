21ee9e38baddbde45c8271a742445b5e079e5a0e
Gbball <48598980+Vezono2@users.noreply.github.com>
2020-05-12 11:03:01 +0300

    Create bot.py

---
+++ bot/bot.py
@@ -0,0 +1,54 @@
+import config
+from modules.BotUtil import BotUtil
+
+bot = BotUtil(config.token, config.creator)
+from pymongo import MongoClient
+
+db = MongoClient(config.database)
+users = db.stickers.users
+
+import hashlib
+
+
+@bot.message_handler(commands=['login'])
+def me_handler(m):
+    user = users.find_one({'id': m.from_user.id})
+    if not user:
+        bot.reply_to(m, 'Вы не зарегестрированы. Нажмите /start')
+        return
+    user_id = str(m.from_user.id)
+    hash = hashlib.sha1(user_id.encode()).hexdigest()
+    hash = hashlib.md5(hash.encode()).hexdigest()
+    link = f'lk-contest.herokuapp.com/index.html?login={hash}'
+    tts = f'Вот ваша ссылка для входа. НИКОМУ ЕЕ НЕ ДАВАЙТЕ!\n{link}'
+    bot.reply_to(m, tts)
+
+
+@bot.message_handler()
+def register_handler(m):
+    if not users.find_one({'id': m.from_user.id}):
+        user_id = str(m.from_user.id)
+        hash = hashlib.sha1(user_id.encode()).hexdigest()
+        hash = hashlib.md5(hash.encode()).hexdigest()
+        user = {
+            'id': m.from_user.id,
+            'hash': hash,
+            'stickers': {
+                'unsorted': []
+            }
+        }
+        users.insert_one(user)
+
+
+@bot.message_handler(content_types=['sticker'])
+def stickers_handler(m):
+    stickers = bot.get_sticker_set(m.sticker.set_name).stickers
+    user = users.find_one({'id': m.from_user.id})
+    if not user:
+        bot.reply_to(m, 'Вы не зарегестрированы. Нажмите /start')
+        return
+    for sticker in stickers:
+        file_id = str(sticker.file_id)
+        user['stickers']['unsorted'].append(file_id)
+    users.update_one({'id': m.from_user.id}, {'$set':user})
+    bot.reply_to(m, 'Стикерпак добавлен.')
