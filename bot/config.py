cc6ad3ef852c6edc912e913c0c858e7591b85796
Gbball <48598980+Vezono2@users.noreply.github.com>
2020-06-20 11:50:02 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,3 @@
 creator = 792414733
 token = '1088457782:AAHnoZ1VBbewxcrv1ai-eKqdMaPVEfD50RE'
 database = 'mongodb+srv://gbball:tttttt000!@database-fcfmt.gcp.mongodb.net/test?retryWrites=true&w=majority'
-heroku_key = '1cc64125-28d6-4c47-904f-4122421d5afd'
